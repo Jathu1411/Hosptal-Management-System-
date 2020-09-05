aba56eb997cc59c883303f3ee929e1d999faa364
Sathees <s.paskaran@yahoo.com>
2020-09-05 17:25:19 +0530

    changed favicon and done some cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
+
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello guys</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <h1>hello guys</h1>
+    </Router>
   );
-}
+};
 
 export default App;
