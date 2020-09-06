68bfad65e1c346866142a18e0ed425d1732c71ec
Sathees <s.paskaran@yahoo.com>
2020-09-06 13:10:49 +0530

    added normal and red button components

--- frontend/src/ticket clerk/pages/TcDashboard.js
+++ frontend/src/ticket clerk/pages/TcDashboard.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 import Box from "../../shared/components/Box";
+import Button from "../../shared/components/Button";
 
 const TcDashboard = () => {
   return (
     <div>
-      <Box heading="Ticket clerk Dashboard"></Box>
+      <Box heading="Ticket clerk Dashboard">
+        <Button text="View All Patients" importance="red"></Button>
+      </Box>
     </div>
   );
 };
